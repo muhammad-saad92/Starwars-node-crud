3c6c1f90157918ae1fa926198dc0d7cd94161875
muhammad-saad92 <mughalll92@gmail.com>
2025-08-17 10:20:54 +0500

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('May node be with you')
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('E:/Web development/StarWars' + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log('Helo')
+})
+
+app.listen(3000, function(){
+    console.log('listening on 3000');
+})
